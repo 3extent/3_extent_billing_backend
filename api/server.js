08a324ea2246d57f4bef91ecf0a500573a14aaca
RK <rk.datasigno@gmail.com>
2025-07-13 11:40:56 +0530

    enabled mongo connection

--- api/server.js
+++ api/server.js
@@ -14,17 +14,17 @@ app.get('/', (req, res) => {
 
 app.use('/api/users', userRoutes);
 
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-//   .then(() => {
-//     console.log('MongoDB connected');
-//     app.listen(process.env.PORT, () => {
-//       console.log(`Server running on port ${process.env.PORT}`);
-//     });
-//   })
-//   .catch((err) => console.error(err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.error(err));
 
 
 // Export the Express app as a serverless function
