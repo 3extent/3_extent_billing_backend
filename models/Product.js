a942ee98477db5895af901f2570d281720895ef0
RK <rk.datasigno@gmail.com>
2025-07-28 10:46:00 +0530

    changes in Product schema

--- models/Product.js
+++ models/Product.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  model: { type: mongoose.Schema.Types.ObjectId, ref: 'Model' },
   imei_number: Number,
   sales_price: Number,
   purchase_price: Number,
-  grade: String
+  grade: String,
+  model: { type: mongoose.Schema.Types.ObjectId, ref: 'Model' }
 });
 
 module.exports = mongoose.model('Product', productSchema);
