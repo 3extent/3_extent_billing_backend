ae4c70c4f017da5edb2c314a8515097b25e64a26
RK <rk.datasigno@gmail.com>
2025-08-20 11:16:09 +0530

    Added billing ID

--- models/Billing.js
+++ models/Billing.js
@@ -1,13 +1,27 @@
 const mongoose = require('mongoose');
 
 const billingSchema = new mongoose.Schema({
+  bill_id: { type: Number, unique: true },
   customer: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
-  total_amount: Number,
+  total_amount: Number, 
   payment_status: String,
   payment_String: String,
   status: String,
   createdAt: String
+});
+
+// Auto-increment bill_id before saving
+billingSchema.pre('save', async function(next) {
+  if (this.isNew && !this.bill_id) {
+    try {
+      const lastBilling = await this.constructor.findOne({}, {}, { sort: { 'bill_id': -1 } });
+      this.bill_id = lastBilling ? lastBilling.bill_id + 1 : 1;
+    } catch (error) {
+      return next(error);
+    }
+  }
+  next();
 });
 
 module.exports = mongoose.model('Billing', billingSchema);
