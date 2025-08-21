d3466c7476b12ed205cda66298b0fbc93312b721
RK <rk.datasigno@gmail.com>
2025-08-21 15:33:36 +0530

    Added Id s for Products and User table

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
+  product_id: { type: Number, unique: true },
   model: { type: mongoose.Schema.Types.ObjectId, ref: 'Model' },
   imei_number: String,
   sales_price: String,
@@ -12,6 +13,19 @@ const productSchema = new mongoose.Schema({
   status: String,
   qc_remark: String,
   createdAt: String
+});
+
+// Auto-increment product_id before saving
+productSchema.pre('save', async function(next) {
+  if (this.isNew && !this.product_id) {
+    try {
+      const lastModel = await this.constructor.findOne({}, {}, { sort: { 'product_id': -1 } });
+      this.product_id = lastModel ? lastModel.product_id + 1 : 1;
+    } catch (error) {
+      return next(error);
+    }
+  }
+  next();
 });
 
 module.exports = mongoose.model('Product', productSchema);
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+  user_id: { type: Number, unique: true },
   name: String,
   password: String,
   contact_number: String,
@@ -9,6 +10,19 @@ const userSchema = new mongoose.Schema({
   gst_number: String,
   email: String,
   role: String
+});
+
+// Auto-increment user_id before saving
+userSchema.pre('save', async function (next) {
+  if (this.isNew && !this.user_id) {
+    try {
+      const lastUser = await this.constructor.findOne({}, {}, { sort: { 'user_id': -1 } });
+      this.user_id = lastUser ? lastUser.user_id + 1 : 1;
+    } catch (error) {
+      return next(error);
+    }
+  }
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
