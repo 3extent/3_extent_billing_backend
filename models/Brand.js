b58aac0badd7dad7c011c1c567a8e2b7527ea183
RK <rk.datasigno@gmail.com>
2025-08-21 15:26:56 +0530

    Brands will have brand_id now

--- models/Brand.js
+++ models/Brand.js
@@ -1,7 +1,21 @@
 const mongoose = require('mongoose');
 
 const brandSchema = new mongoose.Schema({
+  brand_id: { type: Number, unique: true },
   name: String,
+});
+
+// Auto-increment brand_id before saving
+brandSchema.pre('save', async function (next) {
+  if (this.isNew && !this.brand_id) {
+    try {
+      const lastBrand = await this.constructor.findOne({}, {}, { sort: { 'brand_id': -1 } });
+      this.brand_id = lastBrand ? lastBrand.brand_id + 1 : 1;
+    } catch (error) {
+      return next(error);
+    }
+  }
+  next();
 });
 
 module.exports = mongoose.model('Brand', brandSchema);
