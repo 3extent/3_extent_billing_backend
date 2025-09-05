8d986a1f66d3c4049b02a32d90f399e456e8e65c
codadhyay <codadhyay@gmail.com>
2025-09-05 11:01:48 +0530

    Model name changes

--- routes/modelRoutes.js
+++ routes/modelRoutes.js
@@ -25,12 +25,14 @@ router.get('/', async (req, res) => {
       filter.name = { $regex: modelName, $options: 'i' }; // case-insensitive match
     }
 
-    const models = await Model.find(filter).populate('brand');
+    let models = await Model.find(filter).populate('brand');
+    models = models.map(model => {
+      return {
+        ...model,
+        ramStorage: model.ramStorage.map(ram => `${ram.ram} ${ram.storage}GB`)
+      }
+    });
 
-    // const response = models.map((singleModel) => {
-
-    //   return {`${singleModel.name} + ${singleModel.ramStorage}`}
-    // })
 
     res.json(models);
   } catch (err) {
@@ -66,13 +68,18 @@ router.post('/', async (req, res) => {
     const skipped = [];
 
     for (const ram of ramStorageList) {
-      const exists = await Model.findOne({ name, brand: brandId, ramStorage: ram });
+      const exists = await Model.findOne({ 
+        name, 
+        brand: brandId, 
+        'ramStorage.ram': ram.ram, 
+        'ramStorage.storage': ram.storage 
+      });
       if (exists) {
         skipped.push(ram);
         continue;
       }
 
-      const model = new Model({ name: `${name} + ${ram}`, brand: brandId });
+      const model = new Model({ name, brand: brandId, ramStorage: [ram] });
       try {
         await model.save();
         created.push(model);
