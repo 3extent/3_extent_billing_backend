fee708fb593d7ece902004153299cf671f6d89b4
codadhyay <codadhyay@gmail.com>
2025-09-16 12:35:54 +0530

    Billing api changes

--- models/Billing.js
+++ models/Billing.js
@@ -4,7 +4,11 @@ const billingSchema = new mongoose.Schema({
   customer: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
   payable_amount: String,
-  paid_amount: String,
+  paid_amount: [
+    { method: String, amount: String },
+    { method: String, amount: String },
+    { method: String, amount: String }
+  ],
   pending_amount: String,
   profit: String,
   status: String,
