dda8a8d6a6a56a1025cafe5b3d9242626a69debe
codadhyay <codadhyay@gmail.com>
2025-09-20 11:12:30 +0530

    Moment

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const productSchema = new mongoose.Schema({
   model: { type: mongoose.Schema.Types.ObjectId, ref: 'Model' },
