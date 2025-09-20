d47788545720fe59e30899af6ce33ea46dfe4717
codadhyay <codadhyay@gmail.com>
2025-09-20 12:21:18 +0530

    Product status changes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -21,7 +21,7 @@ router.get('/', async (req, res) => {
       filter.grade = { $regex: grade, $options: 'i' }; // partial, case-insensitive match
     }
 
-    if (status.length !== 0) {
+    if (status && status.length !== 0) {
       // Multiple statuses - use $in operator for exact matching
       filter.status = { $in: statusArray };
     }
