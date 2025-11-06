4916982e3e17e30699208ba436e30c5c93bd9d8a
ishwari.kanase <codadhyay@gmail.com>
2025-11-06 12:31:48 +0530

    Handled constant variable for adding brand while adding products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -47,7 +47,7 @@ async function createSingleProduct(productData) {
   const { brand_name, model_name, imei_number, sales_price, purchase_price, grade, engineer_name, accessories, supplier_name, qc_remark, status } = productData;
 
   //validate brand
-  const brand = await Brand.findOne({ name: brand_name });
+  let brand = await Brand.findOne({ name: brand_name });
 
   if (!brand) {
     // Create model if it doesn't exist
