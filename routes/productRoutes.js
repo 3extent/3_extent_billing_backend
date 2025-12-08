98f1a52500b0b6d3af5ffce18dfda6bc4be8d67c
codadhyay <codadhyay@gmail.com>
2025-12-08 15:49:18 +0530

    Handled edit product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -231,10 +231,6 @@ router.put('/:id', async (req, res) => {
   try {
     const { model_name, imei_number, sales_price, purchase_price, grade, engineer_name, accessories, supplier_name, qc_remark, status } = req.body;
 
-    const existingProduct = await Product.findOne({ imei_number });
-    if (existingProduct) {
-      return res.status(400).json({ error: 'IMEI already exists' });
-    }
     const product = await Product.findById(req.params.id);
     if (!product) {
       return res.status(404).json({ error: 'Product not found' });
