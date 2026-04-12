b011e9cc08af453484c0ea8e4b64955319fb6565
codadhyay <codadhyay@gmail.com>
2026-04-12 15:12:36 +0530

    Declared Brand

--- modules/Products/product.controller.mjs
+++ modules/Products/product.controller.mjs
@@ -2,6 +2,7 @@ import moment from 'moment';
 import Product from './Product.mjs';
 import User from '../Users/User.mjs';
 import Model from '../Models/Model.mjs';
+import Brand from '../Brands/Brand.mjs';
 
 import {
   createSingleProduct
